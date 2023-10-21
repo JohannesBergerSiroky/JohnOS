1141d21d3fe14bf91e8b480ea6be82099421b7f9
Johannes Berger Siroky <bergersirokyjohannes5959@gmail.com>
2023-10-21 07:32:35 +0200

    Fixed some errors in the cleanimage file and in the runimageonqemu file

--- build/cleanimage.sh
+++ build/cleanimage.sh
@@ -9,6 +9,6 @@ sudo rm -r /mnt/boot
 sudo umount /mnt
  
 # Unmap the image after having used it with Qemu
-sudo kpartx -d /home/johannes/Documents/jkghkj/build/johannos_kernel_usb.img
+sudo kpartx -d /home/johannes/Documents/jkghkj/build/johnos_kernel_usb.img
 sleep 2
-sudo rm /home/johannes/Documents/jkghkj/build/johannos_kernel_usb.img
+sudo rm /home/johannes/Documents/jkghkj/build/johnos_kernel_usb.img
--- build/runimageonqemu.sh
+++ build/runimageonqemu.sh
@@ -1,3 +1,3 @@
 
-sudo qemu-system-i386 -m 512 -drive file=johannos_kernel_usb.img,id=usbstick,if=none,format=raw -usb -device usb-ehci,id=ehci -device usb-storage,bus=ehci.0,drive=usbstick
+sudo qemu-system-i386 -m 512 -drive file=johnos_kernel_usb.img,id=usbstick,if=none,format=raw -usb -device usb-ehci,id=ehci -device usb-storage,bus=ehci.0,drive=usbstick
 
