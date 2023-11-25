6d192896b165de59196e8cd308848bc738d96225
Johannes Berger Siroky <bergersirokyjohannes5959@gmail.com>
2023-11-25 11:37:45 +0100

    Corrected misalignment in the code in using_acpi.h

--- kernel/power/using_acpi.h
+++ kernel/power/using_acpi.h
@@ -39,17 +39,17 @@ uint32_t rsdt_array[128];
 struct root_system_description_ptr {
 
 
-	    char signature[8];
-	    uint8_t checksum;
-	    char oemid[6];
-	    uint8_t revision;
-	    uint32_t rsdt_address;
-	    uint32_t length;
-	    uint32_t xsdt_addr_high;
-	    uint32_t xsdt_addr_low;
-	    uint8_t ext_checksum;
-	    uint16_t reserved_1;
-	    uint8_t reserved_2;
+        char signature[8];
+        uint8_t checksum;
+        char oemid[6];
+        uint8_t revision;
+        uint32_t rsdt_address;
+        uint32_t length;
+        uint32_t xsdt_addr_high;
+        uint32_t xsdt_addr_low;
+        uint8_t ext_checksum;
+        uint16_t reserved_1;
+        uint8_t reserved_2;
 
 
 } __attribute__((packed));
@@ -57,15 +57,15 @@ struct root_system_description_ptr {
 struct root_system_description_tbl {
 
 
-	    char signature[4];
-	    uint32_t length;
-	    uint8_t revision;
-	    uint8_t checksum;
-	    char oemid[6];
-	    char oemtblid[8];
-	    char oemrevision[4];
-	    char engineerid[4];
-	    char engineer_revision[4];
+        char signature[4];
+        uint32_t length;
+        uint8_t revision;
+        uint8_t checksum;
+        char oemid[6];
+        char oemtblid[8];
+        char oemrevision[4];
+        char engineerid[4];
+        char engineer_revision[4];
 
 
 } __attribute__((packed));
@@ -73,95 +73,95 @@ struct root_system_description_tbl {
 struct fixed_acpi_description_table {
 
 
-	    char signature[4];
-	    uint32_t length;
-	    uint8_t fadt_major_version;
-	    uint8_t checksum;
-	    char oemid[6];
-	    char oem_table_id[8];
-	    char oem_revision[4];
-	    char engineerid[4];
-	    char engineer_revision[4];
-	    uint32_t facs_addr;
-	    uint32_t dsdt_addr;
-	    uint8_t might_be_reserved;
-	    uint8_t preferred_pm_profile; 
-	    uint16_t sci_int;
-	    uint32_t smi_cmd; 
-	    uint8_t acpi_enable;
-	    uint8_t acpi_disable;
-	    uint8_t s4biosreq; 
-	    uint8_t pstate_cnt; 
-	    uint32_t pm1a_evt_blk;
-	    uint32_t pm1b_evt_blk;
-	    uint32_t pm1a_cnt_blk; 
-	    uint32_t pm1b_cnt_blk;
-	    uint32_t pm2_cnt_blk; 
-	    uint32_t pm_tmr_blk;
-	    uint32_t gpe0_blk; 
-	    uint32_t gpe1_blk;
-	    uint8_t pm1_evt_len;
-	    uint8_t pm1_cnt_len;
-	    uint8_t pm2_cnt_len;
-	    uint8_t pm_tmr_len; 
-	    uint8_t gpe0_blk_len;
-	    uint8_t gpe1_blk_len;
-	    uint8_t gpe1_base; 
-	    uint8_t cst_cnt;
-	    uint16_t p_lvl2_lat;
-	    uint16_t p_lvl3_lat;
-	    uint16_t flush_size; 
-	    uint16_t flush_stride;
-	    uint8_t duty_offset;
-	    uint8_t duty_width;
-	    uint8_t day_alarm;
-	    uint8_t mon_alarm;
-	    uint8_t century;
-	    uint16_t iapc_boot_arch;
-	    uint8_t reserved;
-	    uint32_t flags;
-	    uint32_t reset_reg_high_high;
-	    uint32_t reset_reg_high_low;
-	    uint32_t reset_reg_low;
-	    uint8_t reset_value;
-	    uint16_t arm_boot_arch;
-	    uint8_t fadt_minor_version;
-	    uint32_t ext_facs_addr_high;
-	    uint32_t ext_facs_addr_low;
-	    uint32_t ext_dsdt_addr_high;
-	    uint32_t ext_dsdt_addr_low;
-	    uint32_t ext_pm1a_evt_blk_high_high;
-	    uint32_t ext_pm1a_evt_blk_high_low;
-	    uint32_t ext_pm1a_evt_blk_low;
-	    uint32_t ext_pm1b_evt_blk_high_high;
-	    uint32_t ext_pm1b_evt_blk_high_low;
-	    uint32_t ext_pm1b_evt_blk_low;
-	    uint32_t ext_pm1a_cnt_blk_high_high;
-	    uint32_t ext_pm1a_cnt_blk_high_low;
-	    uint32_t ext_pm1a_cnt_blk_low;
-	    uint32_t ext_pm1b_cnt_blk_high_high;
-	    uint32_t ext_pm1b_cnt_blk_high_low;
-	    uint32_t ext_pm1b_cnt_blk_low;
-	    uint32_t ext_pm2_cnt_blk_high_high;
-	    uint32_t ext_pm2_cnt_blk_high_low;
-	    uint32_t ext_pm2_cnt_blk_low;
-	    uint32_t ext_pm_tmr_blk_high_high;
-	    uint32_t ext_pm_tmr_blk_high_low;
-	    uint32_t ext_pm_tmr_blk_low;
-	    uint32_t ext_gpe0_blk_high_high;
-	    uint32_t ext_gpe0_blk_high_low;
-	    uint32_t ext_gpe0_blk_low;
-	    uint32_t ext_gpe1_blk_high_high;
-	    uint32_t ext_gpe1_blk_high_low;
-	    uint32_t ext_gpe1_blk_low;
-	    uint32_t sleep_control_reg_high_high;
-	    uint32_t sleep_control_reg_high_low;
-	    uint32_t sleep_control_reg_low;
-	    uint32_t sleep_status_reg_high_high;
-	    uint32_t sleep_status_reg_high_low;
-	    uint32_t sleep_status_reg_low;
-	    uint32_t hypervisor_vendor_identity_high;
-	    uint32_t hypervisor_vendor_identity_low;
+        char signature[4];
+        uint32_t length;
+        uint8_t fadt_major_version;
+        uint8_t checksum;
+        char oemid[6];
+        char oem_table_id[8];
+        char oem_revision[4];
+        char engineerid[4];
+        char engineer_revision[4];
+        uint32_t facs_addr;
+        uint32_t dsdt_addr;
+        uint8_t might_be_reserved;
+        uint8_t preferred_pm_profile; 
+        uint16_t sci_int;
+        uint32_t smi_cmd; 
+        uint8_t acpi_enable;
+        uint8_t acpi_disable;
+        uint8_t s4biosreq; 
+        uint8_t pstate_cnt; 
+        uint32_t pm1a_evt_blk;
+        uint32_t pm1b_evt_blk;
+        uint32_t pm1a_cnt_blk; 
+        uint32_t pm1b_cnt_blk;
+        uint32_t pm2_cnt_blk; 
+        uint32_t pm_tmr_blk;
+        uint32_t gpe0_blk; 
+        uint32_t gpe1_blk;
+        uint8_t pm1_evt_len;
+        uint8_t pm1_cnt_len;
+        uint8_t pm2_cnt_len;
+        uint8_t pm_tmr_len; 
+        uint8_t gpe0_blk_len;
+        uint8_t gpe1_blk_len;
+        uint8_t gpe1_base; 
+        uint8_t cst_cnt;
+        uint16_t p_lvl2_lat;
+        uint16_t p_lvl3_lat;
+        uint16_t flush_size; 
+        uint16_t flush_stride;
+        uint8_t duty_offset;
+        uint8_t duty_width;
+        uint8_t day_alarm;
+        uint8_t mon_alarm;
+        uint8_t century;
+        uint16_t iapc_boot_arch;
+        uint8_t reserved;
+        uint32_t flags;
+        uint32_t reset_reg_high_high;
+        uint32_t reset_reg_high_low;
+        uint32_t reset_reg_low;
+        uint8_t reset_value;
+        uint16_t arm_boot_arch;
+        uint8_t fadt_minor_version;
+        uint32_t ext_facs_addr_high;
+        uint32_t ext_facs_addr_low;
+        uint32_t ext_dsdt_addr_high;
+        uint32_t ext_dsdt_addr_low;
+        uint32_t ext_pm1a_evt_blk_high_high;
+        uint32_t ext_pm1a_evt_blk_high_low;
+        uint32_t ext_pm1a_evt_blk_low;
+        uint32_t ext_pm1b_evt_blk_high_high;
+        uint32_t ext_pm1b_evt_blk_high_low;
+        uint32_t ext_pm1b_evt_blk_low;
+        uint32_t ext_pm1a_cnt_blk_high_high;
+        uint32_t ext_pm1a_cnt_blk_high_low;
+        uint32_t ext_pm1a_cnt_blk_low;
+        uint32_t ext_pm1b_cnt_blk_high_high;
+        uint32_t ext_pm1b_cnt_blk_high_low;
+        uint32_t ext_pm1b_cnt_blk_low;
+        uint32_t ext_pm2_cnt_blk_high_high;
+        uint32_t ext_pm2_cnt_blk_high_low;
+        uint32_t ext_pm2_cnt_blk_low;
+        uint32_t ext_pm_tmr_blk_high_high;
+        uint32_t ext_pm_tmr_blk_high_low;
+        uint32_t ext_pm_tmr_blk_low;
+        uint32_t ext_gpe0_blk_high_high;
+        uint32_t ext_gpe0_blk_high_low;
+        uint32_t ext_gpe0_blk_low;
+        uint32_t ext_gpe1_blk_high_high;
+        uint32_t ext_gpe1_blk_high_low;
+        uint32_t ext_gpe1_blk_low;
+        uint32_t sleep_control_reg_high_high;
+        uint32_t sleep_control_reg_high_low;
+        uint32_t sleep_control_reg_low;
+        uint32_t sleep_status_reg_high_high;
+        uint32_t sleep_status_reg_high_low;
+        uint32_t sleep_status_reg_low;
+        uint32_t hypervisor_vendor_identity_high;
+        uint32_t hypervisor_vendor_identity_low;
 
 
 } __attribute__((packed));
@@ -169,17 +169,17 @@ struct fixed_acpi_description_table {
 struct m_apic_description_table {
 
 
-	    char signature[4];
-	    uint32_t length;
-	    uint8_t revision;
-	    uint8_t checksum;
-	    char oemid[6];
-	    char oem_table_id[8];
-	    char oem_revision[4];
-	    char engineer_id[4];
-	    char engineer_revision[4];
-	    uint32_t local_interrupt_controller_address;
-	    uint32_t flags;
+        char signature[4];
+        uint32_t length;
+        uint8_t revision;
+        uint8_t checksum;
+        char oemid[6];
+        char oem_table_id[8];
+        char oem_revision[4];
+        char engineer_id[4];
+        char engineer_revision[4];
+        uint32_t local_interrupt_controller_address;
+        uint32_t flags;
 
 
 } __attribute__((packed));
@@ -187,22 +187,22 @@ struct m_apic_description_table {
 struct hpet_acpi_table {
 
 
-	    char signature[4];
-	    uint32_t length;
-	    uint8_t Revision;
-	    uint8_t checksum;
-	    char oemid[6];
-	    char oem_table_id[8];
-	    char oem_revision[4];
-	    char engineerid[4];
-	    char engineer_revision[4];
-	    uint32_t event_timer_block_id;
-	    uint32_t baddress_32low_high_high;
-	    uint32_t baddress_32low_high_low;
-	    uint32_t baddress_32_low;
-	    uint8_t hpet_number;
-	    uint16_t min_clock_tick_counter_pmode;
-	    uint8_t page_protection_oem_attribute;
+        char signature[4];
+        uint32_t length;
+        uint8_t Revision;
+        uint8_t checksum;
+        char oemid[6];
+        char oem_table_id[8];
+        char oem_revision[4];
+        char engineerid[4];
+        char engineer_revision[4];
+        uint32_t event_timer_block_id;
+        uint32_t baddress_32low_high_high;
+        uint32_t baddress_32low_high_low;
+        uint32_t baddress_32_low;
+        uint8_t hpet_number;
+        uint16_t min_clock_tick_counter_pmode;
+        uint8_t page_protection_oem_attribute;
 
 
 } __attribute__((packed));
@@ -210,15 +210,15 @@ struct hpet_acpi_table {
 struct s_system_descriptor_table {
 
 
-	    char signature[4];
-	    uint32_t length;
-	    uint8_t revision;
-	    uint8_t checksum;
-	    char oemid[6];
-	    char oem_table_id[8];
-	    char oem_revision[4];
-	    char engineer_id[4];
-	    char engineer_revision[4];
+        char signature[4];
+        uint32_t length;
+        uint8_t revision;
+        uint8_t checksum;
+        char oemid[6];
+        char oem_table_id[8];
+        char oem_revision[4];
+        char engineer_id[4];
+        char engineer_revision[4];
 
 
 } __attribute__((packed));
@@ -226,15 +226,15 @@ struct s_system_descriptor_table {
 struct d_system_descriptor_table {
 
 
-	    char signature[4];
-	    uint32_t length;
-	    uint8_t revision;
-	    uint8_t checksum;
-	    char oemid[6];
-	    char oem_table_id[8];
-	    char oem_revision[4];
-	    char engineer_id[4];
-	    char engineer_revision[4];
+        char signature[4];
+        uint32_t length;
+        uint8_t revision;
+        uint8_t checksum;
+        char oemid[6];
+        char oem_table_id[8];
+        char oem_revision[4];
+        char engineer_id[4];
+        char engineer_revision[4];
 
 
 } __attribute__((packed));
